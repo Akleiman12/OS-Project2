997ddcbdbb8274281067129be3b4e8f0e554411a
Akleiman12 <asherkleiman@gmail.com>
2016-11-19 21:49:56 -0400

    Creacion de todas las clases necesarias PT2

---
+++ src/rquintero_akleiman/p2/Proceso.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rquintero_akleiman.p2;
+
+/**
+ *
+ * @author Asher y Reynaldo
+ */
+public class Proceso {
+    private int ID;
+    private int recursosMaximos[];
+    
+    
+}
